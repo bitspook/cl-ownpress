fea6d63136557ed1aa23ba235fc5736327f76141
Charanjit Singh <bitspook@protonmail.com>
2022-06-11 17:22:27 +0530

    feat: get org-roam-provider to add inputs into store using rpc

--- src/main.lisp
+++ src/main.lisp
@@ -36,38 +36,37 @@
 
 (defmethod invoke-provider ((provider (eql *org-roam-provider*)) &key)
   (let* ((script-path (asdf:system-relative-pathname "cl-ownpress" "./src/providers/org-roam/org-roam.el"))
-         (server-thread nil))
+         (rpc-channel (lparallel:make-channel)))
+
+    (lparallel:submit-task rpc-channel #'start-rpc-server #'store-input)
 
     (uiop:run-program (format nil "emacs --script ~a" script-path)
                       :output *standard-output*
                       :error-output *standard-output*)
 
-    (when (bt:thread-alive-p server-thread)
-      (bt:destroy-thread server-thread))))
+    (lparallel:receive-result rpc-channel)))
 
-(defun create-rpc-server ()
+(defun start-rpc-server (msg-processor)
   (let* ((socket (usocket:socket-listen #(127 0 0 1) 8192 :reuse-address t))
          (conn (usocket:socket-accept socket :element-type 'character))
          (msg-sep (concatenate 'list "<<<<RPC-EOM>>>>")))
     (unwind-protect
-         (progn
-           (loop
-             :with stream := (usocket:socket-stream conn)
-             :for
-             msg := (str:trim
-                     (str:replace-first msg-sep ""
-                      (with-output-to-string (msg-str)
-                        (loop :named inner
-                          :with trail := '()
-                          :for char := (read-char stream nil 'eof)
-                          :do (princ char msg-str)
-                              (when (>= (length trail) (length msg-sep))
-                                (setq trail (subseq trail 1)))
-                              (setq trail (nconc trail (list char)))
-                              (when (equal trail msg-sep) (return-from inner))))))
-             :until (string= msg "DONE")
-             :collect (yason:parse msg) :into msgs
-             finally (return msgs)))
+         (loop
+           :with stream := (usocket:socket-stream conn)
+           :for
+           msg := (str:trim
+                   (str:replace-first msg-sep ""
+                    (with-output-to-string (msg-str)
+                      (loop :named inner
+                            :with trail := '()
+                            :for char := (read-char stream nil 'eof)
+                            :do (princ char msg-str)
+                                (when (>= (length trail) (length msg-sep))
+                                  (setq trail (subseq trail 1)))
+                                (setq trail (nconc trail (list char)))
+                                (when (equal trail msg-sep) (return-from inner))))))
+           :until (string= msg "DONE")
+           :do (funcall msg-processor (yason:parse msg)))
       (progn
         (usocket:socket-close conn)
         (usocket:socket-close socket)))))
