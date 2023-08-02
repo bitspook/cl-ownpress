af996ff0631c1d8de4bc33626c892a57d5f649b3
Charanjit Singh <bitspook@proton.me>
2023-08-02 04:56:23 +0200

    feat: html-publisher capable of publishing a widget to HTML files

--- src/publishers/html/html.lisp
+++ src/publishers/html/html.lisp
@@ -1,8 +1,23 @@
 (in-package :clown-publishers)
 
+(export-always 'html-publisher)
 (defclass html-publisher (publisher)
-  ((widgets :initarg :widgets
-            :initform nil
-            :accessor publisher-widgets
-            :documentation  "List of widgets which this publisher will use to produce Html/Css/Js"))
-  (:documentation "Base class for publishers which produce Html artifacts."))
+  ((asset-pub :initarg :asset-pub
+              :accessor html-asset-publisher
+              :documentation
+              "`asset-publisher' that will be used to publish produced assets (Html files, Css,
+              fonts, images etc)"))
+  (:documentation "Publish `widget's as Html files along with their dependent assets.
+(Although it is named `html-publisher', it publishes both Html and Css files. It's called
+`html-publisher' because HTML is the primary artifact.)"))
+
+(defmethod publish ((pub html-publisher) &key widget path)
+  "Publish WIDGET at PATH. PATH is relative to DEST. If CLEAN-URL-P is non-nil, final destination is
+DEST/PATH/index.html, else DEST/PATH.html"
+  ;; (format nil "PUBLISING ~a TO ~a" widget path)
+  (let* ((*render-stack* nil)
+         (html (progn
+                (push widget *render-stack*)
+                (with-html-string (dom-of widget))))
+         (ass (html-asset-publisher pub)))
+    (publish ass :path path :content html)))
--- src/publishers/html/html.test.lisp
+++ src/publishers/html/html.test.lisp
@@ -3,3 +3,54 @@
 (use-package :clown-publishers)
 
 (define-test "html-publisher")
+
+(defparameter *ass*
+  (make-instance
+   'cpub:asset-publisher
+   :dest *test-dir*))
+
+(defparameter *pub*
+  (make-instance
+   'cpub:html-publisher
+   :asset-pub *ass*
+   :dest *test-dir*))
+
+(defparameter *test-src-dir* #p"/tmp/clown-test-src/"
+              "Directory where source files to be used in tests should be stored.")
+
+(defun setup ()
+  (uiop:delete-directory-tree *test-dir* :validate t :if-does-not-exist :ignore)
+  (uiop:delete-directory-tree *test-src-dir* :validate t :if-does-not-exist :ignore)
+  (ensure-directories-exist *test-src-dir*))
+
+(define-test "publish" :parent "html-publisher"
+  (define-test "creates html file at given PATH relative to DEST"
+    ;; Simple widget
+    (progn
+      (defwidget btn (title) nil (:button title))
+
+      (setup)
+
+      (publish
+       *pub*
+       :path #p"simple-widgets/simple-button.html"
+       :widget (make-instance 'btn :title "Lol"))
+
+      (true (uiop:file-exists-p (path-join *test-dir* "simple-widgets/simple-button.html")))
+      (true (str:from-file (path-join *test-dir* "simple-widgets/simple-button.html")) "<button>Lol</button>"))
+
+    ;; A bit more complex widget
+    (progn
+      (defwidget btn (title) nil (:button title))
+      (defwidget nav () nil
+        (:nav (render 'btn :title "Rofl")
+              (render 'btn :title "Lmao")
+              (render 'btn :title "Roflmao")))
+      (setup)
+
+      (publish *pub*
+               :widget (make-instance 'nav)
+               :path "pages/nav.html")
+
+      (true (uiop:file-exists-p (path-join *test-dir* "pages/nav.html")))
+      (true (str:from-file (path-join *test-dir* "pages/nav.html")) "<nav><button>Rofl</button><button>Lmao</button><button>Roflmao</button></nav>"))))
