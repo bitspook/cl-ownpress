80e042b8439689a01dab1cc053a605f49c216c43
Charanjit Singh <bitspook@proton.me>
2023-08-08 09:33:52 +0200

    feat: html-publisher capable of producing and using css files
    The API is a bit awkward though. Perhaps when I understand cl macros better I'll be able to come up
    with something neat-er.

--- src/publishers/html/html.lisp
+++ src/publishers/html/html.lisp
@@ -3,7 +3,6 @@
 (export-always 'html-publisher)
 (defclass html-publisher (publisher)
   ((asset-pub :initarg :asset-pub
-              :accessor html-asset-publisher
               :documentation
               "`asset-publisher' that will be used to publish produced assets (Html files, Css,
               fonts, images etc)"))
@@ -11,13 +10,26 @@
 (Although it is named `html-publisher', it publishes both Html and Css files. It's called
 `html-publisher' because HTML is the primary artifact.)"))
 
-(defmethod publish ((pub html-publisher) &key widget path)
-  "Publish WIDGET at PATH. PATH is relative to DEST. If CLEAN-URL-P is non-nil, final destination is
-DEST/PATH/index.html, else DEST/PATH.html"
-  ;; (format nil "PUBLISING ~a TO ~a" widget path)
+(export-always 'html-page)
+(defclass html-page ()
+  ((title :initarg :title
+          :accessor :html-page-title
+          :documentation "Title of the Html page")))
+
+(defmethod publish ((pub html-publisher) &key page-builder root-widget path)
+  "Publish ROOT-WIDGET at PATH using PAGE-RENDERER."
   (let* ((*render-stack* nil)
-         (html (progn
-                (push widget *render-stack*)
-                (with-html-string (dom-of widget))))
-         (ass (html-asset-publisher pub)))
-    (publish ass :path path :content html)))
+         (ass (slot-value pub 'asset-pub)))
+
+    (push root-widget *render-stack*)
+
+    (let* ((html (with-html-string (dom-of root-widget)))
+           (css-file (publish ass :path #p"styles.css"
+                              :content (rendered-css)
+                              :hash-artifact-name-p t))
+           (html (with-html-string (funcall page-builder :css-file css-file :html html))))
+
+      ;; TODO Create a new instance of asset-publisher for publishing the Html file. Use `asset-pub'
+      ;; only for css/js. Do this when asset-publisher has been updated to configure hash-in-name
+      ;; setting at instance level instead of function call level.
+      (publish ass :path path :content html))))
--- src/publishers/html/html.test.lisp
+++ src/publishers/html/html.test.lisp
@@ -23,7 +23,36 @@
   (uiop:delete-directory-tree *test-src-dir* :validate t :if-does-not-exist :ignore)
   (ensure-directories-exist *test-src-dir*))
 
+(defun identity-html-builder (&key css-file html)
+  (spinneret:with-html (:raw html)))
+
 (define-test "publish" :parent "html-publisher"
+  (define-test "creates a css file in `dest' named styles-<content-hash>.css"
+    (defwidget btn (title)
+        '((.button :color red))
+      (:button title))
+    (defwidget nav ()
+        '((.nav :color blue))
+      (:nav (render 'btn :title "Rofl")
+            (render 'btn :title "Lmao")
+            (render 'btn :title "Roflmao")))
+
+    (let* ((src-filename "styles.css")
+           (content ".button{color:red;}.nav{color:blue;}")
+           (expected-filename (cpub::append-content-hash
+                               src-filename
+                               (md5:md5sum-string content)))
+           (*print-pretty* nil))
+      (setup)
+      (publish *pub*
+               :page-builder #'identity-html-builder
+               :root-widget (make-instance 'nav)
+               :path "pages/nav.html")
+
+      (true (uiop:file-exists-p (path-join *test-dir* expected-filename)))
+      (true (str:from-file (path-join *test-dir* expected-filename))
+            ".button{color:red;}.nav{color:blue;}")))
+
   (define-test "creates html file at given PATH relative to DEST"
     ;; Simple widget
     (progn
@@ -33,8 +62,9 @@
 
       (publish
        *pub*
+       :page-builder #'identity-html-builder
        :path #p"simple-widgets/simple-button.html"
-       :widget (make-instance 'btn :title "Lol"))
+       :root-widget (make-instance 'btn :title "Lol"))
 
       (true (uiop:file-exists-p (path-join *test-dir* "simple-widgets/simple-button.html")))
       (true (str:from-file (path-join *test-dir* "simple-widgets/simple-button.html")) "<button>Lol</button>"))
@@ -49,8 +79,54 @@
       (setup)
 
       (publish *pub*
-               :widget (make-instance 'nav)
+               :page-builder #'identity-html-builder
+               :root-widget (make-instance 'nav)
                :path "pages/nav.html")
 
       (true (uiop:file-exists-p (path-join *test-dir* "pages/nav.html")))
-      (true (str:from-file (path-join *test-dir* "pages/nav.html")) "<nav><button>Rofl</button><button>Lmao</button><button>Roflmao</button></nav>"))))
+      (true (str:from-file (path-join *test-dir* "pages/nav.html")) "<nav><button>Rofl</button><button>Lmao</button><button>Roflmao</button></nav>")))
+
+  (define-test "accepts a page-builder to build final html page"
+    (define-test "provides `css-file' and `html' as argument to widget when rendering HTML"
+      (defwidget btn (title)
+          '((.button :color red))
+        (:button title))
+      (defwidget nav ()
+          '((.nav :color blue))
+        (:nav (render 'btn :title "Rofl")))
+      (defwidget root ()
+          '((body :background magenta))
+        (:div (render 'nav)))
+
+      (let* ((src-filename "styles.css")
+             (content ".button{color:red;}.nav{color:blue;}body{background:magenta;}")
+             (expected-css-file (cpub::append-content-hash
+                                 src-filename
+                                 (md5:md5sum-string content)))
+             (*print-pretty* nil)
+             (build-html-page
+               (lambda (&key css-file html)
+                 (spinneret:with-html
+                   (:html
+                    (:head
+                     (:title "Test page")
+                     (:link :href css-file))
+                    (:body (:raw html)))))))
+        (setup)
+
+        (publish *pub*
+                 :page-builder build-html-page
+                 :root-widget (make-instance 'root)
+                 :path "pages/page.html")
+
+        (true (uiop:file-exists-p (path-join *test-dir* expected-css-file)))
+        (true (uiop:file-exists-p (path-join *test-dir* "pages/page.html")))
+        (true (str:from-file (path-join *test-dir* expected-css-file))
+              ".button{color:red;}.nav{color:blue;}body{background:magenta;}")
+        (true (str:from-file (path-join *test-dir* "pages/page.html"))
+              (format nil "<html lang=en><head><meta charset=UTF-8>~
+                           <title>Test page</title>~
+                           <link href=styles-9F61EA.css>~
+                           </head><body>~
+                           <div><nav><button>Rofl</button></nav>~
+                           </div></body></html>"))))))
