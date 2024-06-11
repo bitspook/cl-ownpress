44b1c552480a976124ca603cdff9e921b331a476
Charanjit Singh <bitspook@proton.me>
2024-06-11 10:24:47 +0200

    fix: safer handling of emacs-script args

--- src/provider/emacs.lisp
+++ src/provider/emacs.lisp
@@ -14,15 +14,23 @@ effective)."))
 (defmethod provide-all ((provider emacs-provider) &rest script-args)
   "Starts a jsonrpc server, runs PROVIDER's :script, collects and returns all messages of type :event
 emitted from Emacs script, until message of type :done is emitted."
-  (let ((events nil))
+  (let ((events nil)
+        (arg-strs (mapcar (lambda (arg)
+                            (cond
+                              ((listp arg) (format nil "(list ~{~s ~})" arg))
+                              ((pathnamep arg) (format nil "~s" (namestring arg)))
+                              (t (format nil "~(~s~)" arg))))
+                          script-args)))
 
     (with-slots (script (server rpc-server)) provider
       (with-rpc-server (msg)
         (:case event (push msg events))
         (:finally done)
         (uiop:run-program (format nil "emacsclient -e '(load \"~a\")' ~
-                                       '(main ~{\"~a\" ~})'"
-                                  script script-args)
+                                       '(main ~a)'"
+                                  script
+                                  ;; get script-args list but without surrounding parens
+                                  (str:substring 1 -1 (format nil "~a" arg-strs)))
                           :output *standard-output*
                           :error-output *standard-output*)))
     events))
